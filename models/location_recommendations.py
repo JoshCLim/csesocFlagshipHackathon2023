3b93c5b288ed0b6db3b1900d9d98b101ac089004
William Feng <williamfeng2001@gmail.com>
2023-07-02 00:47:19 +1000

    Retrieve the user preferences & location information from the backend routes

--- models/location_recommendations.py
+++ models/location_recommendations.py
@@ -1,6 +1,19 @@
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import json
+import requests
+
+
+def get_preferences():
+    url = "http://localhost:8000/locations"
+    response = requests.get(url)
+    if response.status_code == 201:
+        locations = response.json()
+        return locations
+    else:
+        print("Error: Failed to retrieve the locations.")
+        return None
+
 
 def data_ingestion():
     data = {
@@ -18,22 +31,25 @@ def data_ingestion():
     data = pd.DataFrame(data)
     return data
 
+
 def data_to_json(data):
     json_data = data.to_json()
     return json_data
+
 
 class UserBasedCollaborativeFiltering:
     def __init__(self, json_data):
         df = pd.read_json(json_data)
         self.data = df.set_index("attraction")
         self.user_similarity = cosine_similarity(self.data)
-    
+
     def get_similar_users(self, user_id, top_n=5):
-        user_scores = list(enumerate(self.user_similarity[user_id])) 
+        user_scores = list(enumerate(self.user_similarity[user_id]))
         user_scores = sorted(user_scores, key=lambda x: x[1], reverse=True)
         similar_users = [user for user, _ in user_scores[1:top_n+1]]
-        
+
         return similar_users
+
 
 if __name__ == "__main__":
     data = data_ingestion()
@@ -42,6 +58,7 @@ if __name__ == "__main__":
     place_id = len(data) - 1
     similar_users = filtering.get_similar_users(place_id)
     counter = 1
-    for value in similar_users: 
-        print(f"The place that is {counter} similar to {place_id} has id: {value}")
+    for value in similar_users:
+        print(
+            f"The place that is {counter} similar to {place_id} has id: {value}")
         counter += 1
--- models/user_recommendation.py
+++ models/user_recommendation.py
@@ -20,31 +20,46 @@ This script is:
 
 NUM_CLASSES = 9
 
+
+def get_preferences():
+    url = "http://localhost:8000/preferences"
+    response = requests.get(url)
+    if response.status_code == 201:
+        preferences = response.json()
+        return preferences
+    else:
+        print("Error: Failed to retrieve user preferences.")
+        return None
+
+
 def data_ingestion():
     data = pd.DataFrame({'user_id': [0, 1, 2, 3, 1, 3],
                         'item1': [5, 2, 1, 4, 1, 3],
-                        'item2': [4, 4, 2, 3, 2, 3],
-                        'item3': [3, 5, 4, 2, 3, 3],
-                        'item4': [1, 2, 5, 1, 3, 3]})
+                         'item2': [4, 4, 2, 3, 2, 3],
+                         'item3': [3, 5, 4, 2, 3, 3],
+                         'item4': [1, 2, 5, 1, 3, 3]})
     data = data.groupby('user_id').mean()
     json_data = data.to_json()
     return json_data
+
 
 class UserBasedCollaborativeFiltering:
     def __init__(self, json_data):
         self.data = pd.read_json(json_data)
         self.user_similarity = cosine_similarity(self.data)
-    
+
     def get_similar_users(self, user_id, top_n=5):
-        user_scores = list(enumerate(self.user_similarity[user_id])) 
+        user_scores = list(enumerate(self.user_similarity[user_id]))
         user_scores = sorted(user_scores, key=lambda x: x[1], reverse=True)
         similar_users = [user for user, _ in user_scores[1:top_n+1]]
-        
+
         return similar_users
-    
+
+
 def get_userid():
-    userid=1
+    userid = 1
     return userid
+
 
 class FineTunedModel(nn.Module):
     def __init__(self, num_classes):
@@ -58,43 +73,41 @@ class FineTunedModel(nn.Module):
         x = self.resnet(x)
         return x
 
-# Call PyTorch Model 
-def image_classification(num_classes = NUM_CLASSES):
+
+def image_classification(num_classes=NUM_CLASSES):
+    # Call PyTorch Model
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),   
-        transforms.ToTensor(),           
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225])
     ])
-    
+
     model = FineTunedModel(num_classes)
     model.load_state_dict(torch.load('model.pth'))
 
     image = Image.open('./test/lunar_park.jpg').convert('RGB')
     image = transform(image)
-    image = image.unsqueeze(0)  
+    image = image.unsqueeze(0)
 
     # Make predictions
     with torch.no_grad():
         output = model(image)
         probabilities = torch.sigmoid(output)
         predicted_labels = (probabilities >= 0.5).squeeze().tolist()
-    
+
     return predicted_labels
+
 
 if __name__ == "__main__":
     json_data = data_ingestion()
     filtering = UserBasedCollaborativeFiltering(json_data)
     user_id = get_userid()
     similar_users = filtering.get_similar_users(user_id)
-    
+
     counter = 1
-    for person in similar_users: 
+    for person in similar_users:
         print(f"The person who is {counter} similar has userid: {person}")
         counter += 1
-    
+
     print(image_classification())
-    
-    
-    
-     
-    
